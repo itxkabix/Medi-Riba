809302c9752e2358472d6ce1ae830a78c5813eab
Kabir Ahmed <169141170+itxkabix@users.noreply.github.com>
2025-11-15 14:38:05 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medi-Riba',
+    version= '0.0.0',
+    author= 'Kabir Ahmed',
+    author_email= 'kabirahmed1119@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
